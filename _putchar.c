3759fbfafb946a1cac84de380fea0d5a18f932f3
faithwangu04 <faithwangu04@gmail.com>
2023-04-12 15:04:40 -0700

    0x0A. C - argc, argv

--- _putchar.c
+++ _putchar.c
@@ -1,9 +1,15 @@
-#include "unistd.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * This is my _putchar.c
+ * _putchar - writes the character c to stdout
+ * @c:Ther character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
  */
-int _putchar.c (char.c)
+int _putchar(char c)
 {
-	return write (STDOUT_FILENO, &c, 1);
+return (write(1, &c, 1));
 }
+
