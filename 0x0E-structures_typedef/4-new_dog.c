fddae891e54f5249ed2370c2f82c2ebfe30e22cd
faithwangu04 <faithwangu04@gmail.com>
2023-04-17 14:29:21 -0700

    4 new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,8 +35,8 @@
  *
  * Return: the pointer to dest
  */
-	char *_strcpy(char *dest, char *src)
-	{
+char *_strcpy(char *dest, char *src)
+		{
 		int len, i;
 
 
@@ -60,16 +60,16 @@
 	}
 
 
-	/**
-	 * new_dog - creates a new dog
-	 * @name: name of the dog
-	 * @age: age of the dog
-	 * @owner: owner of the dog
-	 *
-	 * Return: pointer to the new dog (Success), NULL otherwise
-	 */
-	dog_t *new_dog(char *name, float age, char *owner)
-	{
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
+		idog_t *new_dog(char *name, float age, char *owner)
+		{
 		dog_t *dog;
 		int len1, len2;
 
